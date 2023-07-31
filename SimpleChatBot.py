c54e170e3a32da4898d540f81dfed5083feef18b
manish7017 <manish84398@gmail.com>
2023-07-31 14:59:12 +0530

    Input

--- SimpleChatBot.py
+++ SimpleChatBot.py
@@ -6,10 +6,9 @@ os.environ[
     "_BARD_API_KEY"
 ] = "----------"  # Enter your _Secure-1PSID from www.bard.google.com -> Inspect -> Application -> Cookies
 
-input_text = "How many bones present in a elephant??"
+input_text = input("Ask Me Anything!!!  \n")
 output_text = Bard().get_answer(input_text)["content"]
 
-# Save Output in TxT file.
 with open("myfile.txt", "w") as file1:
     file1.write(input_text + "\n")
     file1.write(output_text)
